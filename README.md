339a73f9a8e09b7693e81977967eda47bc63e527
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-03-08 22:25:43 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# sql
+
+# The-Gatherer-2.0
+You can still access the previous version by changing the branch on Github.
+
+This was made using YOLOv5 and OpenCV. The model is now comaptible with CPU and GPU and it detects automaticly the best option for your computer. You can load your custom YOLO models (exported to **ONNX**) by using your own .onnx file.
+
+On how to use your own data set to train a custom model, for now I'ld recomend following this tutorial for custom training. [https://www.youtube.com/watch?v=GRtgLlwxpc4](https://www.youtube.com/watch?v=GRtgLlwxpc4). I'm also uploading very soon a simple guide to export your .pt model to .onnx so it can run in this version of the bot.
+
+If you want to train and export your own custom onnx model you can follow the steps that are set up in the following Google Colab: https://colab.research.google.com/drive/19kVzBERhRwB1jywcKeJ3dALARNd5-dR7?usp=sharing
+
+You can check out the rewritten version on C++ (with no UI) that also uses Onnx to run inference here: https://github.com/Riczap/The-Gatherer-Cpp
+
+- **Known Issue:** Trying to move the window of The Gatherer 2 while having the Bot Vision activated, will crash the program. (You can move the command prompt at any time without issues)
+
+ - **Note:** The Bot and the Vision are independent, you can have the bot running without the Computer Vision function activated. The model is running on the background whenever you activate either of them.
+ 
+ - **Note:** All of the parameters have default values, so you can leve them blank and it'll work fine.
+
+- Training Data (Demo): https://drive.google.com/drive/u/2/folders/17X_f17WpzoxHMURSj5QIZ4lMUWPImf5V
+
+- Showcase: Note that the following video is of the previous 1.0 version with an outdated GUI. [https://www.youtube.com/watch?v=y669rc18ia4](https://www.youtube.com/watch?v=y669rc18ia4)
+![Showcase](https://user-images.githubusercontent.com/77018982/230541525-271eea09-be75-47e8-be8f-6c8bb133668a.PNG)
+
+## New Features
+I've implemented some quality of live updates so it's easier to use for general purposes.
+- You can choose the resolution that you are currently using for your game manually
+
+![Resolution](https://user-images.githubusercontent.com/77018982/230542772-f769a8ff-7da7-4b67-9fbb-f76bdfd8fa6f.PNG)
+- You can add and select your custom models with a drop down menu
+
+![Models](https://user-images.githubusercontent.com/77018982/230542819-248199e9-3c06-4323-b472-fce2487b5446.PNG)
+- You can now input your desired waiting time between the actions of the bot.
+
+Just remember to click the **Save changes** button after you selected your custom parameters
+![Save](https://user-images.githubusercontent.com/77018982/230543242-8bdbd567-e4e6-493d-bb11-cf7b62abba1e.PNG)
+
+
+## Installation
+To use the new version of The Gatherer you can install the dependencies either in your main python environment, using anaconda or as an executable file.
+
+-Download Tutorial: https://www.youtube.com/watch?v=dljCXzuKTKo
+### Python
+ 1. Clone the repository on GitHub (Download the files).
+ 2. Open a console terminal and run the following command to install all of the dependencies: `pip install -r requirements.txt`
+### Conda
+ 1. Clone the repository on GitHub (Download the files).
+ 2. Install Anaconda: [https://www.anaconda.com/products/distribution](https://www.anaconda.com/products/distribution)
+ 3. Create an Environment using the following command on the anaconda prompt: `conda create -n myenv` (you can choose any name you want for the env)
+ 4. Activate the environment using `conda activate myenv` and open the directory where you downloaded the source code for the bot. Run the following line to install all of the dependencies: `pip install -r requirements.txt`
+ 5. Now you can run the **main.py** file through the conda environment using `python main.py`
+### Executable
+ 1. Download and extract the zip file: https://drive.google.com/file/d/1HImNmd06msfE_RuhBxIzT-rLlXL6LCa5/view?usp=share_link
+ 2. Right click and create a shortcut of **The Gatherer 2.exe** file and move it to your desired location
+ 3. Remeber that you'll need to acces the **models** directory to add new custom models.
+
+## How to Add a Custom Model
+### Exporting to Onnx
+ I'm also finishing up a video tutorial explaining how to export your custom models. And here is a step by step guide on how to do it.
+- Open the Google Colab link and follow the steps: https://colab.research.google.com/drive/1uJMeZP4QbSVuA5TNkfeXQDIpFDHVTeAB?usp=share_link
+ ### Adding the model to The Gatherer 2.0
+ 2. Once you've you custom model as a yolov5.onnx file you can proceede to create a text file with a matching name to the name of your model containing the name of your custom classes.
+ 
+ ![Text File](https://user-images.githubusercontent.com/77018982/230546123-b4ef79b7-b65a-42ce-be44-0ad4ee847e22.PNG)
+ 
+ 3. Move both files into the **models** directory.
+
+Feel free to use the code for your own projects!
+
+If you have any issues and need assistance send me a message or post something on:
+Discord: WanderingEye#0330
+Forum: https://www.unknowncheats.me/forum/usercp.php
+
--- main.py
+++ main.py
@@ -1,104 +1,133 @@
-import threading
-from pyrogram import Client, filters
-from io import BytesIO, StringIO
-from sqlalchemy import TEXT, Column, Numeric, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+import customtkinter as ctk
+from gadgets import *
+from onnx_detextion import *
+from window_capture import WindowCapture
+from bot_thread import *
+import cv2
+import sys
 
-DB_URI = "postgres://gromkzdt:dzj-tytwpkowrpJ7_kBWuasUp7dP-qJ1@rogue.db.elephantsql.com/gromkzdt"
+#Move Albion Client to the corner of the screen
 
+#Bot thread
+go = Move()
+go.start()
 
+# Set custom theme
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
 
-apa = Client(
-    name="sql",
-    api_id=21445722,
-    api_hash="710f18f90849255dd85837d00d5fe85f",
-    bot_token="7144522875:AAEFGYd8mx8p3wvfpBozxW5Ea5L-XEG2y2M",
-)
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
 
+        self.models = filter_models(get_files_in_folder())
+        self.model = "rough_stone.onnx"
+        self.is_cuda = len(sys.argv) > 1 and sys.argv[1] == "cuda"
+        self.net = build_model(self.is_cuda, f"models/{self.model}")
 
+        self.resolution = "1024x720"
+        self.waiting_time = 3.5
+        self.width, self.height = self.resolution.split('x')
+        self.screen_center = [int(self.width)/2, int(self.height)/2]
+        self.lock = threading.Lock()
 
+        self.vision_status = "off"
+        self.bot_status = "off"
+        self.wincap = WindowCapture(None)
 
-def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+        self.class_ids, self.confidences, self.boxes, self.class_list, self.centers = [], [], [], [], []
+        
 
+        self.geometry("370x320")
+        self.title("The Gatherer 2.0 - Wandering Eye")
+        self.iconbitmap("wanderingeye.ico")
 
-BASE = declarative_base()
-SESSION = start()
-
-INSERTION_LOCK = threading.RLock()
-
-
-class Broadcast(BASE):
-    __tablename__ = "broadcast"
-    id = Column(Numeric, primary_key=True)
-    user_name = Column(TEXT)
-
-    def __init__(self, id, user_name):
-        self.id = id
-        self.user_name = user_name
-
-
-Broadcast.__table__.create(checkfirst=True)
-
-
-#  Add user details -
-async def add_user(id, user_name):
-    with INSERTION_LOCK:
-        msg = SESSION.query(Broadcast).get(id)
-        if not msg:
-            usr = Broadcast(id, user_name)
-            SESSION.add(usr)
-            SESSION.commit()
+        self.protocol("WM_DELETE_WINDOW", self.on_close)
 
 
-async def delete_user(id):
-    with INSERTION_LOCK:
-        SESSION.query(Broadcast).filter(Broadcast.id == id).delete()
-        SESSION.commit()
+        def update_vision_status():
+            self.vision_status = self.actions_frame.get_state1()
+            print("Vision status updated to:", self.vision_status)
 
 
-async def full_userbase():
-    users = SESSION.query(Broadcast).all()
-    SESSION.close()
-    return users
+        def update_bot_status():
+            self.bot_status = self.actions_frame.get_state2()
+            print("Bot status updated to:", self.bot_status)
+                
+
+        def update_info():
+            cv2.destroyAllWindows()
+            self.actions_frame.reset_values()
+            self.bot_status="off"
+            self.vision_status="off"
+            
+            print(self.onnx_model_box.get_option())
+            self.model = self.onnx_model_box.get_option()
+            self.net = build_model(self.is_cuda, f"models/{self.model}")          
+            print(f"Using: {self.model}")
+            
+            self.width, self.height = self.game_size_box.get_option().split('x')
+            self.wincap = WindowCapture(None, width=int(self.width), height=int(self.height))
+            print(f"Game resolution: {self.width}x{self.height}")
+            
+            self.waiting_time = float(self.waiting_time_frame.get_value())
+            print(f"Waiting time: {self.waiting_time}\n")
+
+        
+
+        #Creating Objects
+        self.actions_frame = SwitchesFrame(self, name="Actions", text1="Display bot's vision", text2="Gather resources", command_name1 = update_vision_status, command_name2 = update_bot_status)
+        self.game_size_box = DropdownFrame(self, name="Select Window Size", text="Game resolution", default="1024x720" , options=["1024x720","1280x720", "1280x1024", "1366x768", "1600x900", "1680x1050", "1920x1080"])
+        self.onnx_model_box = DropdownFrame(self, name="Select detection model", text="Onnx model", default="rough_stone.onnx", options=self.models)
+        self.update_info_button = ctk.CTkButton(self, text="Save changes", command=update_info)
+        self.waiting_time_frame = SingleEntryFrame(self, header_name="EntryFrame1", name="Waiting Time", text="3.5", default=3.5)
+        
+
+        #Drawing Objects
+        self.actions_frame.grid(row=0, column=0, pady=12, padx=10)
+        self.waiting_time_frame.grid(row=1, column=0, pady=12, padx=10)
+        self.game_size_box.grid(row=0, column=1, pady=12, padx=10)
+        self.onnx_model_box.grid(row=1, column=1, pady=12, padx=10)
+        self.update_info_button.grid(row=2, column=0, padx=20, pady=10)
 
 
-async def query_msg():
-    try:
-        return SESSION.query(Broadcast.id).order_by(Broadcast.id)
-    finally:
-        SESSION.close()
+
+    def bot_gathering(self):
+        if self.bot_status=="on":
+            go.update(self.centers, True, self.waiting_time, self.screen_center)
+        elif self.bot_status=="off":
+            go.update([], False, self.waiting_time, self.screen_center)
+
+    def update_screenshot(self):
+        #Avoid running inference if there are no actions activated
+        if(self.vision_status == "on" or self.bot_status =="on"):
+            self.screenshot = self.wincap.get_screenshot()
+            self.class_ids, self.confidences, self.boxes, self.class_list = results_objects(self.screenshot, self.net, self.model)
+            self.centers = get_center(self.boxes)
+            self.frame = results_frame(self.screenshot, self.class_ids, self.confidences, self.boxes, self.class_list)
+        
+        if (self.vision_status == "on"):
+            cv2.imshow("Computer Vision", self.frame)
+            cv2.waitKey(1)
+            self.after(100,self.update_screenshot)
+            self.after(100,self.bot_gathering)
+            
+        elif self.vision_status == "off":
+            cv2.destroyAllWindows()
+            self.after(100,self.update_screenshot)
+            self.after(100,self.bot_gathering)
+
+            
+            
+    def on_close(self):
+        print("Closing")
+        go.stop()
+        self.destroy()    
 
 
 
+if __name__ == "__main__":
 
-
-@apa.on_message(filters.command("crot"))
-async def test(client, message):
-    memek = []
-    try:
-        query= await query_msg()
-        for row in query:
-            chat_id = int(row[0])
-            memek.append(chat_id)
-        final_output = f"{memek}"
-        if len(final_output) > 4096:
-            filename = "output.txt"
-            with open(filename, "w+", encoding="utf8") as out_file:
-               out_file.write(str(final_output))
-            await message.reply_document(
-                document=filename,
-                disable_notification=True,
-            )
-            await apa.send_message(message.chat.id, len(memek))
-        else:
-            await message.reply(final_output)
-    except Exception as e:
-        print(str(e))
-
-apa.run()
-print("idup")
+    app = App()
+    app.after(100, app.update_screenshot)
+    app.mainloop()
